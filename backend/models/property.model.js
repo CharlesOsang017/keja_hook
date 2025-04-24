c4e73282a9981bf23edde314df3bccdf4b82a00f
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-24 21:35:40 +0300

    Add property Schema

---
+++ backend/models/property.model.js
@@ -0,0 +1,37 @@
+import mongoose, { model } from "mongoose";
+
+const propertySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  price: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  location: {
+    type: String,
+    trim: true,
+  },
+  imgaes: {
+    type: [String],
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  isAvailable: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const Property = mongoose.model("Property", propertySchema);
+export default Property;
---
+++ backend/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: true
+  },
+    role: {
+      type: String,
+      enum: ["tenant", "landlord", "admin"],
+      default: "tenant",
+    },
+ 
+});
+
+const User = mongoose.model("user", userSchema);
+export default User;
