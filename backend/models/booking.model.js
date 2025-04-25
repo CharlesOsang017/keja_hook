035c729939eac9089f891610c3f29a6c3d28d63c
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-25 13:31:17 +0300

    configured routes

--- backend/models/booking.model.js
+++ backend/models/booking.model.js
@@ -5,19 +5,20 @@ const bookingSchema = new mongoose.Schema(
     tenant: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      property: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Property",
-      },
-      status: {
-        type: String,
-        enum: ["pending", "approved", "declined"],
-        default: "pending",
-      },
-      startDate: Date,
-      endDate: Date,
     },
+    property: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Property",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "declined"],
+      default: "pending",
+    },
+    startDate: Date,
+    endDate: Date,
   },
+
   { timeStamps: true }
 );
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from  'dotenv'
 import { connectDB } from './config/db.js'
+import userRouter from './routes/user.route.js'
 
 dotenv.config()
 
 const app = express()
 
+
+app.use("/api/users", userRouter)
 
 const port = process.env.PORT || 6000
 
