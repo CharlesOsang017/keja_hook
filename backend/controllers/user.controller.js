894bff3c0f1cf24a29c52ab914e5fdcdd5b25d31
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-28 17:29:13 +0300

    Create a reset password endpoint

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,6 +4,7 @@ import crypto from "crypto";
 import { generateTokenAndSetCookie } from "../utils/token.js";
 import {
   sendPasswordResetEmail,
+  sendResetSuccessEmail,
   sendVerificationEmail,
   sendWelcomeEmail,
 } from "../mailtrap/emails.js";
@@ -131,14 +132,47 @@ export const forgotPassword = async (req, res) => {
       `${process.env.CLIENT_URL}/reset-password/${resetToken}`
     );
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Password reset link sent to your email",
-      });
+    res.status(200).json({
+      success: true,
+      message: "Password reset link sent to your email",
+    });
   } catch (error) {
     console.error("Error in forgotPassword:", error);
     res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid or expired reset token" });
+    }
+
+    // update password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiresAt = undefined;
+    await user.save();
+
+    await sendResetSuccessEmail(user.email);
+
+    res
+      .status(200)
+      .json({ success: true, message: "Password reset successful" });
+  } catch (error) {
+    console.log("Error in resetPassword ", error);
+    res.status(400).json({ success: false, message: error.message });
   }
 };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { login, logout, register, verifyEmail, forgotPassword } from "../controllers/user.controller.js";
+import {
+  login,
+  logout,
+  register,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
-router.post('/login', login)
-router.post('/logout', logout)
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post('/verify-email', verifyEmail)
-router.post('/forgot-password', forgotPassword)
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
