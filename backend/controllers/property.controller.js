2ac1c4e05ed43502961204426522e1950f915ff1
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-29 16:01:16 +0300

    Configured cloudinary for image upload

--- backend/controllers/property.controller.js
+++ backend/controllers/property.controller.js
@@ -1,10 +1,17 @@
 import Property from "../models/property.model.js"
+import User from "../models/user.model.js";
 
 // Create a property
 export const createProperty = async(req, res)=>{
     const {title, description, price, location} = req.body
     let {images} = req.body;
+    const userId = req.user._id
+
     try {
+        const user = await User.findById(userId)
+        if(!title || !description || !price || !location){
+            return res.status(403).json({message: "All fields are required"})
+        }
         const property = new Property({
             title,
             description,
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,30 @@
-import express from 'express'
-import dotenv from  'dotenv'
-import { connectDB } from './config/db.js'
-import userRouter from './routes/user.route.js'
-import propertyRoute from './routes/property.route.js'
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/user.route.js";
+import propertyRoute from "./routes/property.route.js";
+import { v2 as cloudinary } from "cloudinary";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/users", userRouter)
-app.use("/api/properties", propertyRoute)
+// cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const port = process.env.PORT || 6000
+// middleware
+app.use("/api/users", userRouter);
+app.use("/api/properties", propertyRoute);
 
-app.listen(port, async ()=>{
-    await connectDB()
-    console.log(`The app is running on port ${port}`)
-})
+const port = process.env.PORT || 6000;
+
+app.listen(port, async () => {
+  await connectDB();
+  console.log(`The app is running on port ${port}`);
+});
