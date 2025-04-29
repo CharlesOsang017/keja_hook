e06d2c176af64e9fa067462cbeef3d70622b9607
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-29 10:34:50 +0300

    Set up property route

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ import express from 'express'
 import dotenv from  'dotenv'
 import { connectDB } from './config/db.js'
 import userRouter from './routes/user.route.js'
+import propertyRoute from './routes/property.route.js'
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
+
 app.use("/api/users", userRouter)
+app.use("/api/properties", propertyRoute)
 
 const port = process.env.PORT || 6000
 
