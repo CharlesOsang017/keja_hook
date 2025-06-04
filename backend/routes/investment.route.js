0d329530d8b7fbf83a86ad0a41c412e81f57c15f
CharlesOsang017 <charlesosango02@gmail.com>
2025-06-04 13:53:42 +0300

    Added the callback for the investment controller

--- backend/routes/investment.route.js
+++ backend/routes/investment.route.js
@@ -1,8 +1,14 @@
-import express from 'express'
-import { protectRoute } from '../middleware/protectRoute.js';
-import { initiateInvestment } from '../controllers/investment.controller.js';
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  initiateInvestment,
+  verifyInvestmentPayment,
+  investmentCallback,
+} from "../controllers/investment.controller.js";
 
-const router = express.Router()
-router.post("/invest", protectRoute, initiateInvestment )
+const router = express.Router();
+router.post("/invest", protectRoute, initiateInvestment);
+router.post("/verify", protectRoute, verifyInvestmentPayment);
+router.post("/callback", protectRoute, investmentCallback);
 
 export default router;
